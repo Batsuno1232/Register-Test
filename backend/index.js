0659bcb32a5046ee2b561c367cb53fd783b3ccbb
Kunakorn Somsriagsornsang <Batsu1232@gmail.com>
2025-04-13 10:41:41 +0700

    build env and code file storage

--- backend/index.js
+++ backend/index.js
@@ -13,3 +13,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.__dirname(__filename);
 dotenv.config();
 
+const app = express();
+
+// app.use(express.json()); ใช้งานได้เหมือนกับ bodyparser.json()
+app.use(bodyParser.json({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin"}));
+app.use(morgan());
+app.use(cors());
+const buildPath = path.join(__dirname,'build');
+app.use(express.static());
+
+/*FILE STORAGE*/
+const storage = multer.diskStorage({
+    destination: (req,file,cb) =>{
+        cb(null,"build/assets");
+    },
+    filename: (req, file, cb) =>{
+        cb(null,file.originalname);
+    },
+
+});
+
+const upload = multer({storage});
