43321c516c9839caf60c4ea0b61f1a5bf69cb772
Kunakorn Somsriagsornsang <Batsu1232@gmail.com>
2025-04-16 12:00:34 +0700

    update front end setting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
 function App() {
+  const mode = "dark" //useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
       Hello world
+      </ThemeProvider>
     </div>
   );
 }
---
+++ frontend/src/store/index.js
@@ -0,0 +1,56 @@
+import { createSlice, CreateSlice } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialState = {
+    mode: "light",
+    user: null,
+    profile: null,
+    token: null,
+    post:[]
+}
+export const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers:{
+        setMode:(state)=>{
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin:(state,action)=>{
+            state.user = action.payload.user;
+            state.profile = action.payload.user;
+            state.token = action.payload.user;
+        },
+        setLogout:(state)=>{
+            state.user = null;
+            state.profile = null;
+            state.token = null;
+        },
+        setFriends:(state,action)=>{
+            if(state.user){
+                state.user.friends = action.payload.friends;
+            }else{
+                console.error("user friends non existent :<")
+            };
+        },
+        setProfile:(state,action)=>{
+            state.profile = action.payload.profile;
+        },
+        setPosts:(state,action)=>{
+            state.posts = action.payload.posts;
+        },
+        setPost:(state,action)=>{
+            const updatePosts = state.posts.map((post) => {
+                if(post._id === action.payload.post._id){
+                    return action.payload.post;
+                }
+                return post;
+            });
+            state.posts = updatePosts;
+        },
+
+
+    }
+});
+
+export const { setMode,setLogin,setLogout,setFriends,setProfile,setPost,setPosts} = authSlice.actions;
+export default authSlice.reducer;
